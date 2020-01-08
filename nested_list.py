501bf4588a3dd805d42a8c4a317620e5fd502ea8
sindhu-vedala <meet.lsv@gmail.com>
2020-01-08 22:48:26 +0530

    Min 2 in nested list

--- nested_list.py
+++ nested_list.py
@@ -1,8 +1,11 @@
 if __name__ == '__main__':
-    # for _ in range(int(input())):
-    #     name = input()
-    #     score = float(input())
     listt = [[input(), float(input())] for _ in range(int(input()))] 
     print(listt)
-    print(len(listt))
-    print(max(listt))
+    print("===============================================")
+    max2 = sorted(list(set([score for name, score in listt])))[1]
+    print(max2)
+    print("===============================================")
+    print('\n'.join([name for name,score in sorted(listt) if score == max2]))
+    print("===============================================")
+    print(sorted(list([score for name, score in listt])))
+ 
